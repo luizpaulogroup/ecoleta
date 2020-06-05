baa6293672eee7143d7d8a9cf3f674a1a065f023
luizpaulogroup <bylpgaming@gmail.com>
2020-06-05 00:15:14 -0300

    added MapView

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import { Feather as Icon } from '@expo/vector-icons';
+
+import { useNavigation } from '@react-navigation/native';
+
+import MapView from 'react-native-maps';
+
+import {} from '';
 
 import styles from './styles';
 
 const Points = () => {
+
+    const navigation = useNavigation();
+
     return (
-        <View></View>
+        <>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Icon name="arrow-left" color="#34CB79" size={24} />
+                </TouchableOpacity>
+                <Text style={styles.title}>Bem vindo.</Text>
+                <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+                <View style={styles.mapContainer}>
+                    <MapView style={styles.map} />
+                </View>
+            </View>
+            <View style={styles.itemsContainer}>
+                <TouchableOpacity style={styles.item} onPress={() => { }}>
+                    {/* <Image source={} /> */}
+                </TouchableOpacity>
+            </View>
+        </>
     )
 }
 
